58d5addd5fabe9fdd45d4f2de0b17dda51bc91ac
sindre2 <sindre2.kodehode@gmail.com>
2022-09-20 13:30:23 +0200

    Main files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Home } from "./pages/HomePage";
+import { Smiley } from "./pages/SmileyPage";
+import { Spin } from "./pages/SpinPage";
+import { Route, Link, Routes } from "react-router-dom";
+import { ClockContext } from "./pages/Components/ClockContext";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="home">Home</Link>
+          </li>
+          <li>
+            <Link to="smiley">Smiley</Link>
+          </li>
+          <li>
+            <Link to="spin">Spin</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* ClockContext is made available for all the components inside the provider element. */}
+      <ClockContext.Provider value={{ value, setValue }}>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Smiley" element={<Smiley />} />
+          <Route path="/Spin" element={<Spin />} />
+        </Routes>
+      </ClockContext.Provider>
+    </>
   );
 }
 
