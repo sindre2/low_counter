113e8b368615845a00efa5cb6636f524c2ce4496
sindre2 <sindre2.kodehode@gmail.com>
2022-09-21 09:54:04 +0200

    commits

--- src/pages/Components/ClockContext.js
+++ src/pages/Components/ClockContext.js
@@ -2,7 +2,4 @@ import { createContext } from "react";
 
 // createcontext with the value of "null" that is exported. It works sort of a global "let-variable".
 
-const timerUpdate = new Date();
-const zeTime = `${timerUpdate.getHours()}:${timerUpdate.getMinutes()}:${timerUpdate.getSeconds()}`;
-
 export const ClockContext = createContext(zeTime);
