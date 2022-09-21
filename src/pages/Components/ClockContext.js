3c7a842b1f21ef8406ac5d6d1e3bd9d7a2ce4815
sindre2 <sindre2.kodehode@gmail.com>
2022-09-21 09:47:59 +0200

    Configure React app for deployment to GitHub Pages

--- src/pages/Components/ClockContext.js
+++ src/pages/Components/ClockContext.js
@@ -2,4 +2,7 @@ import { createContext } from "react";
 
 // createcontext with the value of "null" that is exported. It works sort of a global "let-variable".
 
-export const ClockContext = createContext(null);
+const timerUpdate = new Date();
+const zeTime = `${timerUpdate.getHours()}:${timerUpdate.getMinutes()}:${timerUpdate.getSeconds()}`;
+
+export const ClockContext = createContext(zeTime);
